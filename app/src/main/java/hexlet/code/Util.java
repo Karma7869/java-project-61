11960a6a06b5db9b5e300ad429d0d439c3ffb0e4
Karma7869 <vlad78697869@gmail.com>
2024-07-16 23:34:46 +0500

    bug fix

--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -1,7 +1,8 @@
 package hexlet.code;
 
 public class Util {
+    private static final int SIZE_RANDOM = 10;
     public static int getRandomNumber() {
-        return (int) (Math.random() * 10);
+        return (int) (Math.random() * SIZE_RANDOM);
     }
 }
